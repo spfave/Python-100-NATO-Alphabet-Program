3e1cabdc0dc4bea97ea3b89873593893762b3610
sfave <sebpfave@gmail.com>
2020-12-02 20:00:14 -0500

    Generate nato phonetic spelling of input word

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ nato_alphabet_data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet = {row.letter: row.code for (
     index, row) in nato_alphabet_data.iterrows()}
 
-print(nato_alphabet)
-# {"A": "Alfa", "B": "Bravo"}
-
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+nato_spelling = [nato_alphabet[letter] for letter in word]
+print(nato_spelling)
