cc20cf1e141cefd7cda905c2410d1eef1f31cb60
sfave <sebpfave@gmail.com>
2020-12-05 23:22:09 -0500

    Error handling added

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ nato_alphabet_data = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet = {row.letter: row.code for (
     index, row) in nato_alphabet_data.iterrows()}
 
+
 # Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-nato_spelling = [nato_alphabet[letter] for letter in word]
-print(nato_spelling)
+def generate_NATO_spelling():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_spelling = [nato_alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only works with letters in NATO code alphabet")
+        generate_NATO_spelling()
+    else:
+        print(nato_spelling)
+
+
+generate_NATO_spelling()
